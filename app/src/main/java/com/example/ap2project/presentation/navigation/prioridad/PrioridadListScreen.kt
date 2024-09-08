06dc08e396b960394c2ac3f56201b9a253dc6ca4
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-09-08 17:54:30 -0400

    Optimizando imports

--- app/src/main/java/com/example/ap2project/presentation/navigation/prioridad/PrioridadListScreen.kt
+++ app/src/main/java/com/example/ap2project/presentation/navigation/prioridad/PrioridadListScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
