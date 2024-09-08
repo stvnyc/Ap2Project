ffcbe504d91cf701a1c231ac9112ed569565de4a
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-09-08 17:50:40 -0400

    Creando screen para utilizar navegacion

---
+++ app/src/main/java/com/example/ap2project/presentation/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package com.example.ap2project.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Screen {
+    @Serializable
+    data object PrioridadList : Screen()
+    @Serializable
+    data class Prioridad(val prioridadId: Int) : Screen()
+}
