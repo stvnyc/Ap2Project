f8e21ba17f958a863c999b671c7c76ada2413a56
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-06 13:05:34 -0400

    Agregando SwipeToDelete

--- app/src/main/java/com/example/ap2project/presentation/navigation/prioridad/PrioridadListScreen.kt
+++ app/src/main/java/com/example/ap2project/presentation/navigation/prioridad/PrioridadListScreen.kt
@@ -1,28 +1,50 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.ap2project.presentation.navigation.prioridad
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxState
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.ap2project.Data.dao.entities.PrioridadEntity
+import kotlinx.coroutines.delay
 
 @Composable
 fun PrioridadListScreen(
@@ -34,7 +56,9 @@ fun PrioridadListScreen(
     PrioridadBodyListScreen(
         uiState,
         goToPrioridadScreen,
-        createPrioridad
+        createPrioridad,
+        onPrioridadSelected = viewModel::selectedPrioridad,
+        onDelete = viewModel::delete
     )
 }
 
@@ -42,7 +66,9 @@ fun PrioridadListScreen(
 fun PrioridadBodyListScreen(
     uiState: UiState,
     goToPrioridadScreen: (Int) -> Unit,
-    createPrioridad: () -> Unit
+    createPrioridad: () -> Unit,
+    onPrioridadSelected: (Int) -> Unit,
+    onDelete: (PrioridadEntity) -> Unit
 ) {
     Scaffold(
         floatingActionButton = {
@@ -76,12 +102,16 @@ fun PrioridadBodyListScreen(
                 )
                 Text(
                     "Descripción",
-                    modifier = Modifier.weight(1.5f),
+                    modifier = Modifier
+                        .weight(1.5f)
+                        .weight(2.5f),
                     fontSize = 20.sp
                 )
                 Text(
                     "Días",
-                    modifier = Modifier.weight(0.5f),
+                    modifier = Modifier
+                        .weight(0.5f)
+                        .weight(2.5f),
                     fontSize = 20.sp
                 )
             }
@@ -91,8 +121,16 @@ fun PrioridadBodyListScreen(
                     .fillMaxSize()
                     .padding(horizontal = 14.dp)
             ) {
-                items(uiState.prioridades){
-                    PrioridadRow(it,goToPrioridadScreen)
+                items(
+                    uiState.prioridades,
+                    key = {it.prioridadId!! }
+                ) {
+                    SwipeToDeleteContainer(
+                        item = it,
+                        onDelete = onDelete
+                    ) { prioridad ->
+                        PrioridadRow(prioridad, goToPrioridadScreen, onPrioridadSelected)
+                    }
                 }
             }
         }
@@ -102,14 +140,16 @@ fun PrioridadBodyListScreen(
 @Composable
 private fun PrioridadRow(
     it: PrioridadEntity,
-    goToPrioridadScreen:(Int) -> Unit
+    goToPrioridadScreen:(Int) -> Unit,
+    onPrioridadSelected: (Int) -> Unit
 ){
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .padding(14.dp)
             .clickable {
-                goToPrioridadScreen(it.prioridadId?:0)
+                goToPrioridadScreen(it.prioridadId ?: 0)
+                onPrioridadSelected(it.prioridadId ?: 0)
             }
     ) {
         Text(
@@ -129,4 +169,68 @@ private fun PrioridadRow(
         )
     }
     HorizontalDivider()
+}
+
+@Composable
+fun <T> SwipeToDeleteContainer(
+    item: T,
+    onDelete: (T) -> Unit,
+    animationDuration: Int = 500,
+    content: @Composable (T) -> Unit
+) {
+    var isRemoved by remember {
+        mutableStateOf(false)
+    }
+    val state = rememberSwipeToDismissBoxState(
+        confirmValueChange = { value ->
+            if (value == SwipeToDismissBoxValue.EndToStart) {
+                isRemoved = true
+                true
+            } else
+                false
+        }
+    )
+    LaunchedEffect(key1 = isRemoved) {
+        if (isRemoved) {
+            delay(animationDuration.toLong())
+            onDelete(item)
+        }
+    }
+    AnimatedVisibility(
+        visible = !isRemoved,
+        exit = shrinkVertically(
+            animationSpec = tween(durationMillis = animationDuration),
+            shrinkTowards = Alignment.Top
+        ) + fadeOut()
+    ) {
+        SwipeToDismissBox(
+            state = state,
+            backgroundContent = {
+                DeleteBackground(state)
+            },
+            content = {content(item)}
+        )
+    }
+}
+
+@Composable
+fun DeleteBackground(
+    swipeToDismissBoxState: SwipeToDismissBoxState
+) {
+    val color = if (swipeToDismissBoxState.dismissDirection == SwipeToDismissBoxValue.EndToStart) {
+        Color.Red
+    } else Color.Transparent
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color)
+            .padding(16.dp),
+        contentAlignment = Alignment.CenterEnd
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Delete,
+            contentDescription = null,
+            tint = Color.White
+        )
+    }
 }
