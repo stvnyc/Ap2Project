b5f79272d1f1d27aecdbdde4d275c2f681d23f07
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-06 15:47:09 -0400

    Actualizando DB

--- app/src/main/java/com/example/ap2project/Data/dao/database/PrioridadDb.kt
+++ app/src/main/java/com/example/ap2project/Data/dao/database/PrioridadDb.kt
@@ -2,17 +2,25 @@ package com.example.ap2project.Data.dao.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.ap2project.Data.dao.converters.Converters
 import com.example.ap2project.Data.dao.dao.PrioridadDao
+import com.example.ap2project.Data.dao.dao.TicketDao
 import com.example.ap2project.Data.dao.entities.PrioridadEntity
+import com.example.ap2project.Data.dao.entities.TicketEntity
 
 @Database(
     entities = [
-        PrioridadEntity::class
+        PrioridadEntity::class,
+        TicketEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 
+@TypeConverters(Converters::class)
 abstract class PrioridadDb : RoomDatabase(){
     abstract fun prioridadDao(): PrioridadDao
+
+    abstract fun ticketDao(): TicketDao
 }
