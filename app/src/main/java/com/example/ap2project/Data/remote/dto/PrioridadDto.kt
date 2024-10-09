6bc4959b06d2db902e8d6cc27614c81635ff2c0c
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-09 19:00:41 -0400

    Agrega Dto's

---
+++ app/src/main/java/com/example/ap2project/Data/remote/dto/PrioridadDto.kt
@@ -0,0 +1,7 @@
+package com.example.ap2project.Data.remote.dto
+
+data class PrioridadDto(
+    val prioridadId: Int?,
+    val descripcion: String,
+    val diasCompromiso: Int
+)
---
+++ app/src/main/java/com/example/ap2project/Data/remote/dto/SistemaDto.kt
@@ -0,0 +1,6 @@
+package com.example.ap2project.Data.remote.dto
+
+data class SistemaDto (
+    val sistemaId: Int?,
+    val sistemaNombre: String
+)
